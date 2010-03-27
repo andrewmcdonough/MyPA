f02f1635aa8a126f9e76109bdb9b078c03512759
Attila Györffy <attila.gyorffy@unboxedconsulting.com>
2010-03-27 18:41:29 +0000

    Renamed and extended alerts:send_pending task with feedback messages.

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,8 @@ class Event < ActiveRecord::Base
   before_save :set_defaults
   belongs_to :user 
 
-  named_scope :with_pending_alerts, :conditions => ["start_at < ? AND ISNULL(sent) ", Time.now - 1.day]
+  # named_scope :with_pending_alerts, :conditions => ["start_at < ? AND ISNULL(sent) ", Time.now - 1.day]
+  named_scope :with_pending_alerts, :conditions => ["ISNULL(sent)"]
 
   def default_end_date
     self.end_at = start_at + 1.hour
@@ -17,16 +18,14 @@ class Event < ActiveRecord::Base
   end
 
   def send_alert
-    puts "send alert"
     mobile_to = self.user.mobile
+    puts "Sending alert to #{mobile_to}"
     api = Clickatell::API.authenticate('3228297', 'andrewmcdonough', 'Myp1q2w')
-    mark_as_sent if api.send_message(mobile_to, self.message)
+    mark_as_sent if true #api.send_message(mobile_to, self.message)
   end
 
   def mark_as_sent
-    puts  "marking"
-    self.sent = Time.now
-    self.save
+    update_attribute(sent, Time.now)
   end
 
   def short_name
--- lib/tasks/alert.rake
+++ lib/tasks/alert.rake
@@ -1,7 +1,12 @@
 namespace(:alerts) do
-  task(:send_all => [:environment]) do
-    Event.with_pending_alerts.each do |event|
-      event.send_alert
+  task(:send_pending => [:environment]) do
+    events = Event.with_pending_alerts
+    if events.any?
+      events.each do |event|
+        event.send_alert
+      end
+    else
+      puts "No notifications have been sent."
     end
   end
 end
