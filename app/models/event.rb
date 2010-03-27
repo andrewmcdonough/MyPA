c138cf2f212be416f36f603dae6f5f271fe95867
Andrew McDonough <andrew.mcdonough@unboxedconsulting.com>
2010-03-27 14:42:48 +0000

    event default end date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,13 @@
 class Event < ActiveRecord::Base
   has_event_calendar
-  
+
+  before_save :default_end_date
+
+
+  def default_end_date
+    self.end_at = start_at + 1.hour
+  end
+
 end
 # == Schema Information
 #
