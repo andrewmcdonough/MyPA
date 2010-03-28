a238d35ce1ec4464c9fcb80f25c1796d1d121ad5
Attila Györffy <attila.gyorffy@unboxedconsulting.com>
2010-03-28 12:21:14 +0100

    Main navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def contextual_link_class(active_controller_name)
+    return (controller.controller_name == active_controller_name) ? 'active' : nil
+  end
+  
 end
