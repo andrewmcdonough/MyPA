0c7232f5b9d3a996eb63fe777a30df6f3b6143cc
Attila Györffy <attila.gyorffy@unboxedconsulting.com>
2010-03-28 11:13:17 +0100

    Home controller is back and is the default route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    redirect_to calendar_path(:year => Date.today.year, :month => Date.today.month) if current_user
+    #redirect_to calendar_path(:year => Date.today.year, :month => Date.today.month) if current_user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'calendar', :action => 'index', :year => Time.zone.now.year, :month => Time.zone.now.month
+  # map.root :controller => 'calendar', :action => 'index', :year => Time.zone.now.year, :month => Time.zone.now.month
+  map.root :controller => 'home'
 
   # See how all your routes lay out with "rake routes"
 
